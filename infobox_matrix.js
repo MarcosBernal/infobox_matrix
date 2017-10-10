4085bf7eb69035a4f09d7f310d121c4443be6366
MarcosBernal <marcosbernal3@hotmail.com>
2017-10-10 02:24:17 +0200

    Added animation for sliding the infobox. Simplified the js/css animation with hover

--- infobox_matrix.js
+++ infobox_matrix.js
@@ -1,26 +1,18 @@
 // When DOM has been loaded the function is triggered
 $(document).ready(function() {
-    var speed = 100;
+    var speed = 200;
     $("._infobox").on("mouseenter", function () {
         var default_size = $(this).css("--inside-box-dim").replace('%', '') / 100;
         var default_size_value = default_size * $(this).width()*0.8;
 
         $(this).children().children("._topbar_inside_box").stop().animate(
-            {left: $(this).parent("._inside_box").width()-$(this).width()}, speed, function () {
-                $(this).animate({width: $(this).parent("._inside_box").width(), left: 0}, speed * 2)
-            });
+            {left: 0, width: $(this).children("._inside_box").width()}, speed);
         $(this).children().children("._rightbar_inside_box").stop().animate(
-            {bottom: 0}, speed, function () {
-                $(this).animate({height: $(this).parent("._inside_box").height(),top:0}, speed * 2)
-            });
+            {top: 0,height: $(this).children("._inside_box").height()}, speed);
         $(this).children().children("._bottombar_inside_box").stop().animate(
-            {left: 0}, speed, function () {
-            $(this).animate({width: $(this).parent("._inside_box").width()}, speed * 2)
-        });
+            {left: 0, width: $(this).children("._inside_box").width()}, speed);
         $(this).children().children("._leftbar_inside_box").stop().animate(
-            {top: 0}, speed, function () {
-            $(this).animate({height: $(this).parent("._inside_box").height()}, speed * 2)
-        });
+            {top: 0,height: $(this).children("._inside_box").height()}, speed);
     });
 
     $("._infobox").on("mouseleave", function () {
@@ -33,18 +25,25 @@ $(document).ready(function() {
         $(this).children().children("._topbar_inside_box").stop().animate({
             width: default_width,
             left: default_width_2
-        }, speed * 2);
+        }, speed);
         $(this).children().children("._rightbar_inside_box").stop().animate({
             height: default_height,
             top: default_height_2
-        }, speed * 2);
+        }, speed);
         $(this).children().children("._bottombar_inside_box").stop().animate({
             width: default_width,
             left: default_width_2
-        }, speed * 2);
+        }, speed);
         $(this).children().children("._leftbar_inside_box").stop().animate({
             height: default_height,
             top: default_height_2
-        }, speed * 2);
+        }, speed);
+    });
+
+    $("._infobox").on("click", function () {
+        var position = $(this).offset().left;
+        position = (position < window.outerWidth/2) ? (window.outerWidth - position - $(this).width() -50) : 50;
+
+        $(this).animate({left:position}, speed*2);
     });
 });
